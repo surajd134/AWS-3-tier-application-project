c4ffa147123df00aa505dffc20b740c7b0788922
Suraj Divate <119672864+surajd134@users.noreply.github.com>
2025-06-21 20:40:53 +0530

    Add files via upload

---
+++ AWS-3-Tier-Project/3-Tier Architecture Project-withoutHTTPS/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.c1wo6imeo7vb.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Root123456',
+    DB_DATABASE : 'webappdb'
+});
